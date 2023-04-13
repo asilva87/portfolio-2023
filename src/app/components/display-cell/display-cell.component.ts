19347933db588c9dc779ff82c457b87433184124
Andre Silva <dev01766@proton.me>
2023-04-13 21:52:54 +0200

    Finish display cell component

--- src/app/components/display-cell/display-cell.component.ts
+++ src/app/components/display-cell/display-cell.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core'
+
+import displayMappings from './display-cell-mappings'
 
 @Component({
-  selector: 'app-display-cell',
-  templateUrl: './display-cell.component.html',
-  styleUrls: ['./display-cell.component.scss']
+	selector: 'app-display-cell',
+	templateUrl: './display-cell.component.html',
+	styleUrls: ['./display-cell.component.scss'],
 })
-export class DisplayCellComponent {
+export class DisplayCellComponent implements OnChanges {
+	@Input() numberToDisplay!: string
 
+	public aRow: boolean[] = [false, false, false, false, false]
+	public bRow: boolean[] = [false, false, false, false, false]
+	public cRow: boolean[] = [false, false, false, false, false]
+	public dRow: boolean[] = [false, false, false, false, false]
+	public eRow: boolean[] = [false, false, false, false, false]
+	public fRow: boolean[] = [false, false, false, false, false]
+	public gRow: boolean[] = [false, false, false, false, false]
+
+	constructor() {}
+
+	ngOnChanges(changes: SimpleChanges) {
+		if (changes.numberToDisplay && this.numberToDisplay !== '') {
+			const displayMap = displayMappings[this.numberToDisplay]
+
+			this.aRow = displayMap[0]
+			this.bRow = displayMap[1]
+			this.cRow = displayMap[2]
+			this.dRow = displayMap[3]
+			this.eRow = displayMap[4]
+			this.fRow = displayMap[5]
+			this.gRow = displayMap[6]
+		}
+	}
 }
