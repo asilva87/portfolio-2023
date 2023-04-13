2a3da1c43cdc1046e7b26af4bd1be96dea877575
Andre Silva <dev01766@proton.me>
2023-04-13 21:51:31 +0200

    Add route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,28 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
-import { MengenlehreuhrComponent } from './pages/mengenlehreuhr/mengenlehreuhr.component';
-import { RomanNumeralsConverterComponent } from './pages/roman-numerals-converter/roman-numerals-converter.component';
-import { ScriptConverterComponent } from './pages/script-converter/script-converter.component';
+import { CalculatorComponent } from './pages/calculator/calculator.component'
+import { MengenlehreuhrComponent } from './pages/mengenlehreuhr/mengenlehreuhr.component'
+import { RomanNumeralsConverterComponent } from './pages/roman-numerals-converter/roman-numerals-converter.component'
+import { ScriptConverterComponent } from './pages/script-converter/script-converter.component'
 
 const routes: Routes = [
-  { path: 'mengenlehreuhr', component: MengenlehreuhrComponent },
-  { path: 'roman-numerals-converter', component: RomanNumeralsConverterComponent },
-  { path: 'script-converter', component: ScriptConverterComponent },
-];
+	{ path: 'mengenlehreuhr', component: MengenlehreuhrComponent },
+	{ path: 'roman-numerals-converter', component: RomanNumeralsConverterComponent },
+	{ path: 'script-converter', component: ScriptConverterComponent },
+	{ path: 'calculator', component: CalculatorComponent },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
 
-// This is the main routing module of an Angular application. It imports the Angular NgModule decorator, 
+// This is the main routing module of an Angular application. It imports the Angular NgModule decorator,
 // as well as the RouterModule and Routes interfaces from @angular/router.
 
-// The "routes" constant defines the routes for the application. These are used to map URLs to components, 
+// The "routes" constant defines the routes for the application. These are used to map URLs to components,
 // and to determine which component should be displayed when a user navigates to a particular URL.
 
 // The AppRoutingModule class is an NgModule that defines the "routes" constant, imports the RouterModule
@@ -31,7 +33,7 @@ export class AppRoutingModule { }
 // The "forRoot()" method is a convention used in Angular to provide a configuration for a module that is
 // intended to be imported into the root application module (AppModule). It is called this way to emphasize
 // that the method is intended to be called only once, at the root of the application.
-// In the case of the RouterModule, calling "forRoot()" with the application's routes enables the router 
+// In the case of the RouterModule, calling "forRoot()" with the application's routes enables the router
 // service to start listening for URL changes and route navigation events. The RouterModule is a singleton
 // service that should be imported into the "root module" only once, hence the use of "forRoot()".
 
